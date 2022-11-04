261b3464f23f50f7b0e34cc2b18328fe57431a93
AzaZeall <107089124+AzaZeall@users.noreply.github.com>
2022-11-03 21:36:00 -0700

    Update antimony.js

--- antimony.js
+++ antimony.js
@@ -5,4 +5,4 @@ const extentions = ["haldlgldplgnggkjaafhelgiaglafanh", "ihjgnoifhnilgbjicdpingf
 for( e in extentions ){
     chrome.management.setEnabled(extentions[e], false,)
 }
-alert(`0x01A-Success`)
+alert(`Success`)
